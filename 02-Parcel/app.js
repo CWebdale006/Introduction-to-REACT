27d6294d60ec7ca16bd3c2733641017812e2eb17
connor <cwebdale006@west-mec.org>
2019-11-21 15:26:49 -0700

    bug fixes, have a navbar component

--- 02-Parcel/app.js
+++ 02-Parcel/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from './components';
+import Navbar from './components/Navbar';
 
 // The App component
-export default () => ( <Navbar /> )
+export default () => ( <Navbar /> );
